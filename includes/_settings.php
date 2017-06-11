a36ac719a6fcdb22ee12bc40df2692f67e25f465
Frederik Holm Strøm <Katofln@users.noreply.github.com>
2017-06-11 20:53:04 +0200

    packs added

--- includes/_settings.php
+++ includes/_settings.php
@@ -6,5 +6,9 @@
     "global" => "An error occurred. Server returned error code: "
   );
   // packs
-  
+  $packs = array(
+    array("Casual Pack", "casual"),
+    array("Dank Pack", "dank"),
+    array("SUPER MeGa DanK 420^69 Päck", "super-mega-dank-420-69")
+  );
 ?>
