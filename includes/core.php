aa0b612179b9c447676db0d094f5fcf2548876e4
Frederik Holm Strøm <Katofln@users.noreply.github.com>
2017-06-11 20:45:12 +0200

    update settings

--- includes/core.php
+++ includes/core.php
@@ -2,5 +2,5 @@
   // paths isset check
   isset($pathToHtdocs, $pathToRoot) OR die();
   // includes to core
-  require_once($pathToRoot."includes/settings.php");
+  require_once($pathToRoot."includes/_settings.php");
 ?>
