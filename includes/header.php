7a5e7a55322b414ed986d97958586feded5512fb
Frederik Holm Strøm <Katofln@users.noreply.github.com>
2017-08-27 17:14:23 +0200

    making full javascript

--- includes/header.php
+++
@@ -1,8 +0,0 @@
-<body>
-  <nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container">
-      <div class="navbar-header">
-        <a class="navbar-brand" href="<?php echo $pathToHtdocs; ?>">Kekshop</a>
-      </div>
-    </div>
-  </nav>
