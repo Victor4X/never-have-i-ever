51205fb5185eac480afba6c17bb4fa32ff99b16b
Katofln <frederikhstroem@gmail.com>
2018-01-21 16:44:47 +0100

    Added some more questions

--- js/gamePacks/da_hyggeligFest.js
+++ js/gamePacks/da_hyggeligFest.js
@@ -5,6 +5,17 @@ var da_hyggeligFest = {
 // Questions
 
 "Jeg har aldrig onaneret",
-"Jeg har aldrig røget hash"
+"Jeg har aldrig røget hash",
+"Jeg har aldrig været på Roskilde",
+"Jeg har aldrig kastet op på grund af alkohol",
+"Jeg har aldrig været et dumt svin over for nogen",
+"Jeg har aldrig ville slå nogen",
+"Jeg har adrig slået nogen (IKKE bare stille og rolig søskende vold)",
+"Jeg har aldrig sovet i timerne",
+"Jeg har aldrig spillet et instrument",
+"Jeg har aldrig fået stress",
+"Jeg har aldrig løjet for mine forældre",
+"Jeg har aldrig været på skiferie",
+"Jeg har aldrig været kristen"
 
 ]};
